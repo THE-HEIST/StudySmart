1396d6a3853b8153908c2bbc942b2fe2e6bb2e20
Ho Quoc Bao <blake@blakes-MacBook-Air.local>
2026-08-28 09:43:44 +0700

    Implement priority calculation and ranking

---
+++ src/core/common (Hùng Anh & Bảo)/calculate_priority.py
@@ -0,0 +1,31 @@
+
+def calculate_priority_score (assignment):
+    difficulty = assignment["difficulty"]
+    days_remaining = assignment["days_remaining"]
+    priority_score = difficulty / (days_remaining + 1)
+    return priority_score
+
+def get_priority_level (priority_score):
+    if priority_score >= 2:
+        priority_level = "HIGH"
+    elif priority_score < 1:
+        priority_level = "LOW"
+    else:
+        priority_level = "MEDIUM"
+    return priority_level
+
+def view_priority_ranking(assignments):
+    unfinished_assignments = []
+    for assignment in assignments:
+        if assignment["completed"] == False:
+            unfinished_assignments.append (assignment)
+    if unfinished_assignments == []:
+        print ("Không có danh Task nào chưa hoàn thành")
+        return unfinished_assignments
+    ranked_assignments = sorted(unfinished_assignments , key= calculate_priority_score, reverse=True)
+    for assignment_number, assignment in enumerate(ranked_assignments, start=1):
+        priority_score = calculate_priority_score(assignment)
+        priority_level = get_priority_level (priority_score)
+        assignment_name = assignment["assignment_name"]
+        print (assignment_number, assignment_name, priority_score, priority_level)
+    return ranked_assignments
